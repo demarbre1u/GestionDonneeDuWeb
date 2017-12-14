a43e418a408829fac1ec12c295c7c05a62fbd8d1
Allan Demarbre <demarbreallan@gmail.com>
2017-12-14 08:20:33 +0100

    Petite modif des CSS, gestion des erreurs dans les cas où on ne pourrait pas récupérer les données des parkings, la base de données est maintenant initialisées au démarrage du serveur, puis toute les 30 minutes, ajout de commentaires

--- Server.js
+++ Server.js
@@ -27,11 +27,13 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + "/public/html/index.html"));
 });
 
-app.get('/updatedb', (req, res) =>
+let updatedb = () =>
 {
+    // On récupère en AJAX les données de nos parkings
     axios.get("https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson")
     .then( response =>
     {
+        // On se connecte à notre base de données Mongo
         MongoClient.connect("mongodb://localhost:27017/test", (error, db) => 
         {
             if(error) console.log(error);
@@ -40,6 +42,7 @@ app.get('/updatedb', (req, res) =>
             
             parkings.remove();
        
+            // Pour chaque parking récupéré, on l'insère dans notre base de données
             response.data.features.forEach(element => 
             {
                 let toInsert = 
@@ -57,13 +60,23 @@ app.get('/updatedb', (req, res) =>
 
             db.close();
         });
+    }).catch( (error) =>
+    {
+        console.log("There was a problem while trying to fetch data. The service you're trying to access may be down.");
     });
+}
+
+// Route pour forcer l'update de la base de données
+app.get('/updatedb', (req, res) =>
+{
+    updatedb();  
 
     res.redirect("/");
 })
 
 app.post('/location', (req, res) => 
 {
+    // On récupère la liste des parkings
     let parkingTab = [];    
     MongoClient.connect("mongodb://localhost:27017/test", (err, db) =>
     {
@@ -78,15 +91,18 @@ app.post('/location', (req, res) =>
         });
     });    
 
+    // On récupère l'adresse dans la requête
     var geocodeParams = 
     {
         "address": req.body.adresse
     };
        
+    // On récupère la latitude et la longitude de notre adresse via l'API GoogleMap
     gmAPI.geocode(geocodeParams, function(err, result)
     {
         if(result.results.length > 0)
         {
+            // On trie notre tableau en fonction de la proximité des parkings par rapport à notre adresse
             parkingTab.sort( (a, b) => 
             {
                 let location = result.results[0].geometry.location;
@@ -98,6 +114,7 @@ app.post('/location', (req, res) =>
             })
         }
             
+        // On render notre page avec les 3 parkings les plus proches
         res.render('location', {
             adresseValide: result.results[0],
             p1: parkingTab[0], 
@@ -106,5 +123,12 @@ app.post('/location', (req, res) =>
         })
     });
 });
+
+// On update la base de données au démarrage du serveur, puis toutes les 30min
+updatedb();
+setInterval( () =>
+{
+    updatedb();
+}, 1000 * 60 * 30);
 
 app.listen(8080);
